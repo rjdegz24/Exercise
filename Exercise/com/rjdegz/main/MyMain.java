61eb28d88388f104e6ca994eb3e17d349ef2dbd2
rjdeguzman <robertdeguzman@deltek.com>
2021-06-08 19:48:20 +0800

    add test script

--- Exercise/com/rjdegz/main/MyMain.java
+++ Exercise/com/rjdegz/main/MyMain.java
@@ -10,9 +10,24 @@ public class MyMain {
 		mazda.transmission();
 		mazda.horn();
 		mazda.gear();
+		
+		System.out.println(getRecursiveSumOfDigit("2147283647", 2147283647));
 	}
 
+	public static int getRecursiveSumOfDigit(String n, int k) {
+	    int iSum = 0;
+	        for (int i = 0; i < k; i++) {
+	            n += n;
+	        }
+	        for (int i = 0; i < n.length(); i++) {
+	            iSum += n.charAt(i) - '0';
+	        }
+	            return (iSum >= 10) ? getRecursiveSumOfDigit(String.valueOf(iSum), 1) : iSum;
+	}
 }
+
+
+
 
 
 class Mazda implements Cars {
